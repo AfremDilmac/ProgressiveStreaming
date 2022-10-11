81eab3e7636e101be685edb40ff9f3af1a560f36
Anthony2159 <anthony.pornel@student.odisee.be>
2022-10-11 12:32:36 +0200

    update

--- videosdk-rtc-react-app/src/constants/index.js
+++ videosdk-rtc-react-app/src/constants/index.js
@@ -74,18 +74,14 @@ export const stats = [
   {
     id: "stats-1",
     title: "User Active",
-    value: "3800+",
+    value: "0+",
   },
   {
     id: "stats-2",
     title: "Trusted by Company",
-    value: "230+",
+    value: "0+",
   },
-  {
-    id: "stats-3",
-    title: "Transaction",
-    value: "$230M+",
-  },
+  
 ];
 
 export const footerLinks = [
