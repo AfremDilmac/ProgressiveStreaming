a19695aaff72b5a869aabd74841645bea51537fb
Anthony2159 <anthony.pornel@student.odisee.be>
2022-10-11 13:56:01 +0200

    update

--- videosdk-rtc-react-app/src/components/Navbar.js
+++ videosdk-rtc-react-app/src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <h1 className="text-4xl font-extrabold dark:text-white">streaME</h1>
+      <h1 className="text-4xl font-extrabold dark:text-white">StreaME</h1>
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
--- videosdk-rtc-react-app/src/constants/index.js
+++ videosdk-rtc-react-app/src/constants/index.js
@@ -47,25 +47,25 @@ export const feedback = [
   {
     id: "feedback-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
+      "Very Good, Very nice",
+    name: "Anthony Pornel ",
     title: "Founder & Leader",
     img: people01,
   },
   {
     id: "feedback-2",
     content:
-      "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
+      "Love the statistics",
+    name: "Afrem Dilmac",
     title: "Founder & Leader",
     img: people02,
   },
   {
     id: "feedback-3",
     content:
-      "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
-    title: "Founder & Leader",
+      "Yes Very Good started this without any friends and at the end still no new friends",
+    name: "Kenn Barbie",
+    title: "User",
     img: people03,
   },
 ];
